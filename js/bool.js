2464c226248ad1b4d3479af0bbb23b88316b8f32
akbarinfo <girtbezbet@gmail.com>
2022-03-02 10:02:54 +0500

    update

---
+++ js/bool.js
@@ -0,0 +1,91 @@
+// Bool 1
+
+// let ason = 3;
+// let bson = 4;
+
+// let result = ason == bson ? true : false;
+
+// console.log(result);
+
+
+// Bool 2
+
+// let ason = 9;
+
+
+// let result = ason == 7 ? true : false;
+
+// console.log(result);
+
+// Bool 3
+
+// let ason = -6;
+
+// let result = ason > 0 ? true : false;
+
+// console.log(result);
+
+// Bool 4
+
+// let ason = 3;
+
+// let result = ason < 0 ? true : false;
+
+// console.log(result);
+
+// Bool 5
+
+// let ason = 7;
+
+// let result = ason % 2 == 1 ? true : false;
+
+// console.log(result);
+
+
+// Bool 6
+
+// let ason = 8;
+
+// let result = ason % 2 == 0 ? true : false;
+
+// console.log(result);
+
+
+// Bool 7
+
+// let ason = 3;
+// let bson = 4;
+// let cson = 5;
+
+// let result = ason < bson  && bson < cson ? true : false;
+
+// console.log(result);
+
+// Bool 8
+
+// let ason = 4;
+// let bson = 3;
+
+// let result = ason > 0 && bson > 0 ? true : false;
+
+// console.log(result);
+
+
+// Bool 9
+
+// let ason = -4;
+// let bson = -3;
+
+// let result = ason < 0 && bson < 0 ? true : false;
+
+// console.log(result);
+
+
+// Bool 10
+
+// let ason = 5;
+// let bson = 4;
+
+// let result = ason % 2 == 0 && bson % 2 == 0 ? true : false;
+
+// console.log(result);
